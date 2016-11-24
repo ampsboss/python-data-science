fc7db6b34d328aca9925a99b31379bd879498c05
iegor <yehorboss@gmail.com>
2016-11-24 23:53:17 +0200

    Added query input, moved config values to .ini file

--- Twitter Sentiment Analysis.py
+++ Twitter Sentiment Analysis.py
@@ -1,20 +1,31 @@
 import tweepy
 from textblob import TextBlob
-import twitterConfig
+import configparser
+
+config = configparser.ConfigParser()
+config.read('twitterConfig.ini')
 
 # Step 1 - Authenticate
-consumer_key = twitterConfig.consumer_key
-consumer_secret = twitterConfig.consumer_secret
-access_token = twitterConfig.access_token
-access_token_secret = twitterConfig.access_token_secret
+consumer_key = config['twitter API credentials']['consumer_key']
+consumer_secret = config['twitter API credentials']['consumer_secret']
+access_token = config['twitter API credentials']['access_token']
+access_token_secret = config['twitter API credentials']['access_token_secret']
 
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 
 api = tweepy.API(auth)
 
+# Step 2 - Getting a query
 # Step 3 - Retrieve Tweets
-public_tweets = api.search('MacBook',lang = 'en')
+while True:
+    try:
+        query = input('Enter a query')
+        public_tweets = api.search(query,lang = 'en')
+        break
+    except tweepy.error.TweepError:
+        print('Invalid input. Please try again: ')
+
 
 # CHALLENGE - Instead of printing out each tweet, save each Tweet to a CSV file
 # and label each one as either 'positive' or 'negative', depending on the sentiment
